c2b892fc0fe3617f3f154fd1c35c491d0143deee
2580063862 <2580063862@qq.com>
2022-03-10 23:01:19 +0800

    练习

---
+++ 水仙花/ConsoleApplication/ConsoleApplication.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num =100;
+	while (num<1000)
+	{
+		/*
+			pow(a,b)次方，表示a^b，num / 100百位数，num /10 % 10十位数
+			num % 10个位数
+		*/
+		if ((pow(num / 100, 3) + pow(num /10 % 10, 3) + pow(num % 10, 3))==num) {
+			cout << num << endl;
+		}
+		num++;
+	}
+}
+
